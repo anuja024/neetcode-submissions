bfd0fb2854ef601b62f3a7ed09d177fcddc53388
anuja024 <157204064+anuja024@users.noreply.github.com>
2026-09-05 21:37:38 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string rev = "";
+        for(char c: s){
+            if(isalnum(c)){
+                rev+= tolower(c);
+            }
+        }
+        string str=rev;
+        reverse(str.begin(), str.end());
+        return str == rev;
+    }
+};
